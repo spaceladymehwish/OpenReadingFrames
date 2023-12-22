d4bf710a27666071a439313ccd138037f4cf6ff2
Spacelady Mehwish <mrehman.msbi23sines@student.nust.edu.pk>
2023-12-22 12:35:35 +0500

    orf_finder assignment

--- orf_finder.py
+++ orf_finder.py
@@ -3,15 +3,64 @@ import os
 import sys
 
 #Your Code Here
-class ORFFinder:
-    def __init__
-    # Correctly initialize class
+def find_longest_orf(sequence):
+    start_codon = "ATG"
+    stop_codons = ["TAG", "TGA", "TAA"]
+    longest_orf = ""
+    current_orf = ""
 
-    def read_fasta_file   # Implement code to read fasta file and return sequences
+    for i in range(len(sequence)):
+        if sequence[i:i+3] == start_codon:
+            current_orf = start_codon
 
-    def find_longest_orf  # Implement code to find the longest DNA ORF in a given sequence
+            for j in range(i+3, len(sequence), 3):
+                codon = sequence[j:j+3]
+                current_orf += codon
 
-    def process_sequences #Implement code to process all sequences and find the overall longest DNA ORF
-        pass
+                if codon in stop_codons:
+                    if len(current_orf) > len(longest_orf):
+                        longest_orf = current_orf
+                    break 
 
-def main # initialize to let various arguments pass
+    return longest_orf
+
+def main():
+    if len(sys.argv) < 2:
+        print("Please provide the input file path as an argument.")
+        return
+
+    file_path = sys.argv[1]
+
+    if not os.path.isfile(file_path):
+        print("Invalid file path.")
+        return
+
+    with open(file_path, 'r') as file:
+        sequences = {}
+        current_sequence = ""
+
+        for line in file:
+            line = line.strip()
+
+            if line.startswith(">"):
+                if current_sequence:
+                    sequences[current_sequence_name] = current_sequence
+                    current_sequence = ""
+
+                current_sequence_name = line[1:]
+            else:
+                current_sequence += line
+
+        if current_sequence:
+            sequences[current_sequence_name] = current_sequence
+
+    for sequence_name, sequence in sequences.items():
+        longest_orf = find_longest_orf(sequence)
+
+        print(f"Longest ORF in sequence '{sequence_name}': {longest_orf}")
+
+        if "--print_length" in sys.argv:
+            print(f"Length: {len(longest_orf)}")
+
+if __name__ == "__main__":
+    main()
